ef94d803443b51505f5b1befe0aa106fb0111992
Afonso  Pereira <afonsopereira@Mac-mini-de-Afonso.local>
2020-01-15 15:56:54 -0300

    commit message

---
+++ src/main.js
@@ -0,0 +1,72 @@
+import api from "./api";
+
+class App {
+  constructor() {
+    this.repositories = [];
+
+    this.formEl = document.getElementById("form-id");
+    this.repoListEl = document.getElementById("repo-list");
+    this.inputEl = document.querySelector("input[name=repository]");
+
+    this.registerHandlers();
+  }
+
+  registerHandlers() {
+    this.formEl.onsubmit = event => this.addRepository(event);
+  }
+
+  async addRepository(event) {
+    event.preventDefault();
+
+    const repoInput = this.inputEl.value;
+
+    if (repoInput.lenght == 0) return;
+
+    const response = await api.get(`/repos/${repoInput}`);
+
+    console.log(response);
+
+    const {
+      name,
+      description,
+      html_url,
+      owner: { avatar_url }
+    } = response.data;
+
+    this.repositories.push({
+      name,
+      description,
+      avatar_url,
+      html_url
+    });
+    this.render();
+  }
+
+  render() {
+    this.repoListEl.innerHTML = "";
+    this.repositories.forEach(repo => {
+      let imgEl = document.createElement("img");
+      imgEl.setAttribute("src", repo.avatar_url);
+
+      let titleEl = document.createElement("strong");
+      titleEl.appendChild(document.createTextNode(repo.name));
+
+      let descriptionEl = document.createElement("p");
+      descriptionEl.appendChild(document.createTextNode(repo.description));
+
+      let linkEl = document.createElement("a");
+      linkEl.setAttribute("target", "_blank");
+      linkEl.appendChild(document.createTextNode("Acessar"));
+
+      let listItemEl = document.createElement("li");
+      listItemEl.appendChild(imgEl);
+      listItemEl.appendChild(titleEl);
+      listItemEl.appendChild(descriptionEl);
+      listItemEl.appendChild(linkEl);
+
+      this.repoListEl.appendChild(listItemEl);
+    });
+  }
+}
+
+new App();
