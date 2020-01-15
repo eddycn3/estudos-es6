51806c229df3900439fa81a05daba2967105ae63
Afonso  Pereira <afonsopereira@Mac-mini-de-Afonso.local>
2020-01-15 18:00:13 -0300

    done

--- src/main.js
+++ src/main.js
@@ -15,31 +15,54 @@ class App {
     this.formEl.onsubmit = event => this.addRepository(event);
   }
 
+  setLoading(loading = true) {
+    if (loading === true) {
+      let loadingEl = document.createElement("span");
+      loadingEl.appendChild(document.createTextNode("Carregando"));
+      loadingEl.setAttribute("id", "loading");
+
+      this.formEl.appendChild(loadingEl);
+    } else {
+      document.getElementById("loading").remove();
+    }
+  }
+
   async addRepository(event) {
     event.preventDefault();
 
     const repoInput = this.inputEl.value;
 
-    if (repoInput.lenght == 0) return;
+    console.log(repoInput);
+    if (repoInput === "") return;
 
-    const response = await api.get(`/repos/${repoInput}`);
+    this.setLoading();
 
-    console.log(response);
+    try {
+      const response = await api.get(`/repos/${repoInput}`);
 
-    const {
-      name,
-      description,
-      html_url,
-      owner: { avatar_url }
-    } = response.data;
+      console.log(response);
 
-    this.repositories.push({
-      name,
-      description,
-      avatar_url,
-      html_url
-    });
-    this.render();
+      const {
+        name,
+        description,
+        html_url,
+        owner: { avatar_url }
+      } = response.data;
+
+      this.repositories.push({
+        name,
+        description,
+        avatar_url,
+        html_url
+      });
+
+      this.inputEl.value = "";
+
+      this.render();
+    } catch (error) {
+      alert("O repositório não existe");
+    }
+    this.setLoading(false);
   }
 
   render() {
@@ -56,13 +79,19 @@ class App {
 
       let linkEl = document.createElement("a");
       linkEl.setAttribute("target", "_blank");
+      linkEl.setAttribute("href", repo.html_url);
       linkEl.appendChild(document.createTextNode("Acessar"));
 
+      let divEl = document.createElement("div");
+
       let listItemEl = document.createElement("li");
-      listItemEl.appendChild(imgEl);
-      listItemEl.appendChild(titleEl);
-      listItemEl.appendChild(descriptionEl);
-      listItemEl.appendChild(linkEl);
+
+      divEl.appendChild(imgEl);
+      divEl.appendChild(titleEl);
+      divEl.appendChild(descriptionEl);
+      divEl.appendChild(linkEl);
+
+      listItemEl.appendChild(divEl);
 
       this.repoListEl.appendChild(listItemEl);
     });
